80d7706bdac9cf6eea128774ac96ffe70ce02a84
Zoultoetu <antoine.fayolle2@etu.univ-st-etienne.fr>
2024-11-12 23:12:51 +0100

    test script

--- script.sh
+++ script.sh
@@ -6,6 +6,47 @@ cd ~/SAE5.02/Deploiement_de_machine
 docker-compose up
 touch ~/SAE5.02/Deploiement_de_machine/inventaire.ini
 cat <<final > ~/SAE5.02/Deploiement_de_machine/inventaire.ini
+    [dns]
+    192.168.0.2
+    
+    [ad]
+    192.168.0.3
+
+    [ldap]
+    192.168.0.4
+
+    [home_assistant]
+    192.168.0.5
+
+    [openvpn]
+    192.168.0.6
+
+    [opnsense]
+    192.168.0.7
+
+    [client]
+    192.168.0.8
+
+    [all:vars]
+    ansible_connection=docker
+final
+
+cat <<final > /etc/hosts
+    192.168.0.2 dns 
+    
+    192.168.0.3 ad
+    
+    192.168.0.4 ldap
+    
+    192.168.0.5 home_assistant
+    
+    192.168.0.6 openvpn
+    
+    192.168.0.7 opnsense
+    
+    192.168.0.8 client
 
 final
+
+ansible-playbook -i ~/SAE5.02/Deploiement_de_machine/inventaire.ini ~/main.yml
 
