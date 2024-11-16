97df168efc2196f587fed35364f51387f85d72fc
Zoultoetu <antoine.fayolle2@etu.univ-st-etienne.fr>
2024-11-17 00:28:12 +0100

    compléter inevtaire.ini

--- script.sh
+++ script.sh
@@ -6,34 +6,6 @@ cd /home/toine-fa/SAE5.02/Deploiement_de_machine
 
 docker-compose up -d
 
-cat <<EOF > /home/toine-fa/SAE5.02/Deploiement_de_machine/inventaire.ini
-[dns]
-192.168.0.2
-
-[ad]
-192.168.0.3
-
-[ldap]
-192.168.0.4
-
-[home_assistant]
-192.168.0.5
-
-[openvpn]
-192.168.0.6
-
-[opnsense]
-192.168.0.7
-
-[client]
-192.168.0.8
-
-[all:vars]
-ansible_connection=ssh
-ansible_user=root
-ansible_ssh_pass=root_password
-EOF
-
 cat <<EOF > /etc/hosts
 192.168.0.2    dns
 192.168.0.3    ad
