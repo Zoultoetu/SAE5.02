134063205c699186ac9cdefa058ef8dd1be0c61b
Zoultoetu <antoine.fayolle2@etu.univ-st-etienne.fr>
2024-11-17 11:24:20 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -20,8 +20,8 @@ printf "test ping"
 ansible all -i /home/toine-fa/SAE5.02/Deploiement_de_machine/inventaire.ini -m ping
 printf "test connexion ssh"
 ansible-playbook -i /home/toine-fa/SAE5.02/Deploiement_de_machine/inventaire.ini install_ssh.yml
-printf "test débogage"
-ansible-playbook -i /chemin/vers/inventaire.ini install_ssh.yml -vvvv
+#printf "test débogage"
+#ansible-playbook -i /chemin/vers/inventaire.ini install_ssh.yml -vvvv
 
-cd /home/toine-fa/SAE5.02/playbook  # Déplacement vers le dossier contenant les playbooks
-ansible-playbook -i /home/toine-fa/SAE5.02/Deploiement_de_machine/inventaire.ini main.yml
+#cd /home/toine-fa/SAE5.02/playbook  # Déplacement vers le dossier contenant les playbooks
+#ansible-playbook -i /home/toine-fa/SAE5.02/Deploiement_de_machine/inventaire.ini main.yml
