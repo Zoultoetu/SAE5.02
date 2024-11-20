43e1cfb99df31a2c41e38d278520eed56512ca10
Zoultoetu <antoine.fayolle2@etu.univ-st-etienne.fr>
2024-11-20 22:39:38 +0100

    test chat

--- test_ansible/script.sh
+++ test_ansible/script.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-cd /home/toine-fa/
-#git clone https://github.com/Zoultoetu/SAE5.02
-cd /home/toine-fa/SAE5.02/test_ansible
+# Répertoire de base
+BASE_DIR="/home/toine-fa/SAE5.02/test_ansible"
+
+# Réinitialiser les conteneurs et volumes
+docker rm -f bind9 2>/dev/null || true
+docker volume prune -f 2>/dev/null || true
+
+# Préparer le projet
+cd "$BASE_DIR"
 docker-compose build
 docker-compose up -d
 
+# Exécuter Ansible
 ansible-playbook -i inventory.yml playbook.yml
+
 
 #printf "\n test ping"
 #ansible-playbook -i inventory.yml playbook.yml --extra-vars "ansible_user=toine-fa"
