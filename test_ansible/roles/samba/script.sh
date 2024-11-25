de7e1b502b02d3927cda90dd4048432769c4c257
Zoultoetu <antoine.fayolle2@etu.univ-st-etienne.fr>
2024-11-25 21:55:14 +0100

    test

--- test_ansible/roles/samba/script.sh
+++ test_ansible/roles/samba/script.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Répertoire de base
-BASE_DIR="./SAE5.02/test_ansible/roles/bind9"
+# Répertoire de base pour Samba AD
+BASE_DIR="./SAE5.02/test_ansible/roles/samba"
 
-# Réinitialiser les conteneurs et volumes
-docker rm -f bind9 2>/dev/null || true
+# Réinitialiser les conteneurs et volumes liés à Samba AD
+docker rm -f ad 2>/dev/null || true
 docker volume prune -f 2>/dev/null || true
 
-# Préparer le projet
+# Construire et démarrer le conteneur AD
 cd "$BASE_DIR"
 docker-compose build
 docker-compose up -d
 
-# Exécuter Ansible
+# Exécuter Ansible pour configurer l'AD
 ansible-playbook -i inventaire.ini playbook.yml
 
+# Redémarrer le conteneur pour appliquer les configurations
 docker stop ad
-
 docker start ad
