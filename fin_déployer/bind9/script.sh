669787ed231e73c932680e959e99e991ffd77cb3
Zoultoetu <antoine.fayolle2@etu.univ-st-etienne.fr>
2024-11-25 19:04:48 +0100

    remise correctement

---
+++ fin_déployer/bind9/script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Répertoire de base
+BASE_DIR="./SAE5.02/fin_déployer/bind9"
+
+# Réinitialiser les conteneurs et volumes
+docker rm -f bind9 2>/dev/null || true
+docker volume prune -f 2>/dev/null || true
+
+# Préparer le projet
+cd "$BASE_DIR"
+docker-compose build
+docker-compose up -d
+
+# Exécuter Ansible
+ansible-playbook -i inventaire.ini playbook.yml
+
+docker stop bind9
+
+docker start bind9
