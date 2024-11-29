b747275e5fa374bd153588e545ef9e3edc16bdfd
Zoultoetu <antoine.fayolle2@etu.univ-st-etienne.fr>
2024-11-29 19:59:46 +0100

    Update script.sh

--- test_ansible/script.sh
+++ test_ansible/script.sh
@@ -4,15 +4,15 @@
 BIND9_DIR="./SAE5.02/test_ansible/roles/bind9"
 SAMBA_DIR="./SAE5.02/test_ansible/roles/samba"
 
-# echo "=== Réinitialisation des conteneurs et volumes Bind9 ==="
-# cd "$BIND9_DIR"
-# pwd
-# docker rm -f bind9 2>/dev/null || true
-# docker volume prune -f 2>/dev/null || true
+echo "=== Réinitialisation des conteneurs et volumes Bind9 ==="
+cd "$BIND9_DIR"
+pwd
+docker rm -f bind9 2>/dev/null || true
+docker volume prune -f 2>/dev/null || true
 
-# echo "=== Préparation et démarrage de Bind9 ==="
-# docker-compose build
-# docker-compose up -d
+echo "=== Préparation et démarrage de Bind9 ==="
+docker-compose build
+docker-compose up -d
 
 echo "=== Passage à la configuration de Samba AD ==="
 cd "/home/toine-fa/$SAMBA_DIR"
@@ -26,10 +26,10 @@ docker-compose up -d
 echo "=== Exécution de la configuration DNS et AD avec Ansible ==="
 
 cd ../../../.. # Retour au répertoire racine du projet
-# pwd
-# ansible-playbook -i ./SAE5.02/test_ansible/inventaire.ini "$BIND9_DIR/playbook.yml"
-# docker stop dns
-# docker start dns
+pwd
+ansible-playbook -i ./SAE5.02/test_ansible/inventaire.ini "$BIND9_DIR/playbook.yml"
+docker stop dns
+docker start dns
 pwd
 
 ansible-playbook -i ./SAE5.02/test_ansible/inventaire.ini "$SAMBA_DIR/playbook.yml" -vvv
