bab8b264478d8066caf0a80108ca31389a6ec913
Zoultoetu <antoine.fayolle2@etu.univ-st-etienne.fr>
2024-12-03 18:50:39 +0100

    e

--- test_fusion/script.sh
+++ test_fusion/script.sh
@@ -2,20 +2,23 @@
 
 BASE_DIR="./SAE5.02/test_fusion"
 
-echo "=== Déploiement automatique ==="
+echo "=== Déploiement automatique de Bind9 et Samba ==="
 
-# Étape 1 : Réinitialisation
-echo "=== Réinitialisation des conteneurs ==="
-docker rm -f fusion_service 2>/dev/null || true
+# Étape 1 : Réinitialisation des conteneurs
+echo "=== Réinitialisation des conteneurs et volumes ==="
+docker rm -f dns_ad 2>/dev/null || true
 docker volume prune -f 2>/dev/null || true
-pwd
+
 # Étape 2 : Construction et démarrage du conteneur
 echo "=== Construction et démarrage du conteneur ==="
 cd "$BASE_DIR" || exit
 docker-compose up -d --build
-cd
-pwd
-# Étape 3 : Exécution du playbook Ansible
+
+# Étape 3 : Pause pour s'assurer que le conteneur est prêt
+echo "=== Attente du démarrage du conteneur ==="
+sleep 10
+cd 
+# Étape 4 : Exécution du playbook Ansible
 echo "=== Configuration des services avec Ansible ==="
 ansible-playbook -i "$BASE_DIR/inventaire.ini" "$BASE_DIR/playbook.yml"
 
