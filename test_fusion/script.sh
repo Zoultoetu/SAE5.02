2ee69ad3d7674ed3766b27f428efddc2e0e45264
Zoultoetu <antoine.fayolle2@etu.univ-st-etienne.fr>
2024-12-03 18:45:57 +0100

    Update script.sh

--- test_fusion/script.sh
+++ test_fusion/script.sh
@@ -8,12 +8,12 @@ echo "=== Déploiement automatique ==="
 echo "=== Réinitialisation des conteneurs ==="
 docker rm -f fusion_service 2>/dev/null || true
 docker volume prune -f 2>/dev/null || true
-
+pwd
 # Étape 2 : Construction et démarrage du conteneur
 echo "=== Construction et démarrage du conteneur ==="
 cd "$BASE_DIR" || exit
 docker-compose up -d --build
-
+pwd
 # Étape 3 : Exécution du playbook Ansible
 echo "=== Configuration des services avec Ansible ==="
 ansible-playbook -i "$BASE_DIR/inventaire.ini" "$BASE_DIR/playbook.yml"
