93b3c5424cdc71b4ff11cd43053760ea69c8371e
Zoultoetu <antoine.fayolle2@etu.univ-st-etienne.fr>
2024-12-20 13:46:20 +0100

    finis

---
+++ ad_windows copy/script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+BASE_DIR="./SAE5.02/ad_windows"
+
+echo "=== Déploiement automatique de la machine VPN ==="
+
+# Étape 1 : Réinitialisation des conteneurs
+echo "=== Réinitialisation des conteneurs et volumes ==="
+docker rm -f vpn_ad 2>/dev/null || true
+docker volume prune -f 2>/dev/null || true
+
+pwd
+# Étape 2 : Construction et démarrage du conteneur
+echo "=== Construction et démarrage du conteneur ==="
+cd "$BASE_DIR" || exit
+sudo docker compose up -d --build
+
+# Étape 3 : Pause pour s'assurer que le conteneur est prêt
+echo "=== Attente du démarrage du conteneur ==="
+sleep 10
+
+# Étape 4 : Configuration avec Ansible
+echo "=== Configuration des services avec Ansible ==="
+cd ../..
+pwd
+ansible-playbook -i "$BASE_DIR/inventaire.ini" "$BASE_DIR/playbook.yml" -e "samba_username=root samba_password=root"
+
+echo "=== Déploiement terminé ==="
+
+echo "=== Copie du Fichier OVPN ==="
+echo root | scp root@192.168.0.2:/etc/openvpn/client.ovpn ./client.ovpn
+
+echo "=== Copie du Fichier OVPN dans le presse papier ==="
+xclip -sel clip < client.ovpn
